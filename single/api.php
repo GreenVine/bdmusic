f08299aed886c12103197c212a72057b7d311342
Robin <greenvine@users.noreply.github.com>
2015-01-21 16:33:53 +1100

    Update api.php
    Removed ExtendedFunc class

--- single/api.php
+++ single/api.php
@@ -1,7 +1,6 @@
 <?php
 	
 	require_once "classes/core.class.php";
-	require_once "classes/extended.class.php";
 	require_once "classes/array2xml.class.php";
 	
 	$bdmusic = new BaiduMusic($_REQUEST);
